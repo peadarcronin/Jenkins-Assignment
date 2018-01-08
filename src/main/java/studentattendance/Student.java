68ca7fc06b7c98825465262ec8f1bb61b5b79eee
Peter Cronin <peadarcronin1@gmail.com>
2018-01-08 14:58:52 +0000

    Update Student.java

--- src/main/java/studentattendance/Student.java
+++ src/main/java/studentattendance/Student.java
@@ -9,10 +9,10 @@ public class Student {
 
     public AttendanceGrade getAttendanceGrade(int attendance) {
         if (attendance < 0 || attendance > 100) throw new IllegalArgumentException();
+        if (attendance == 0) return AttendanceGrade.ABSENT;
         if (attendance < 30) return AttendanceGrade.VERY_POOR;
-        if (attendance < 50) return AttendanceGrade.POOR;
         if (attendance < 70) return AttendanceGrade.AVERAGE;
-        if (attendance <= 90) return AttendanceGrade.GOOD;
+        if (attendance < 90) return AttendanceGrade.GOOD;
         return AttendanceGrade.VERY_GOOD;
     }
 }
